7770849b2f5462b8f61cdae002cd0b3ac3dc2b59
Amir Elmesiry <mrelmesiry@gmail.com>
2025-12-27 21:13:40 +0100

    Fix: Fatal error of leaving ctask directory after installing ctask bin

--- install.sh
+++ install.sh
@@ -3,22 +3,25 @@
 dirname="ctask"
 
 echo 'Cloning git repository...'
-git clone "https://github.com/Amir-jpg-png/$dirname.git"
+eval git clone "https://github.com/Amir-jpg-png/$dirname.git"
 
 echo "Moving into source directory..."
-cd ctask || exit 1
+eval cd ctask || exit 1
 
 echo "Building makefiles into build..."
-cmake -S . -B build
+eval cmake -S . -B build
 
 echo "Moving into build directory"
-cd build || exit 1
+eval cd build || exit 1
 
 echo "Compiling application..."
-make
+eval make
 
 echo "The following will require sudo permissions. "
 
 eval sudo mv ../bin/ctask /usr/local/bin/
 
 echo "The script was successfully installed. "
+
+echo "Removing leftover artifacts..."
+eval cd ../../ && rm -rf ctask || exit 1
