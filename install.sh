391bfe2e11882f4f9c6217ff692ff9c16ea78909
Amir Elmesiry <mrelmesiry@gmail.com>
2025-12-27 20:54:22 +0100

    implement install script

--- install.sh
+++ install.sh
@@ -2,16 +2,23 @@
 
 dirname="ctask"
 
+echo 'Cloning git repository...'
 git clone "https://github.com/Amir-jpg-png/$dirname.git"
 
+echo "Moving into source directory..."
 cd ctask || exit 1
 
+echo "Building makefiles into build..."
 cmake -S . -B build
 
-cd build
+echo "Moving into build directory"
+cd build || exit 1
 
+echo "Compiling application..."
 make
 
-cd ../bin
+echo "The following will require sudo permissions. "
 
-./ctask add test
+eval sudo mv ../bin/ctask /usr/local/bin/
+
+echo "The script was successfully installed. "
